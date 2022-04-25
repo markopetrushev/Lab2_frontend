4f46de5b9a62cad5f07737af645af855e7b6d1fc
Петрушев Марко <marko.petrushev@students.finki.ukim.mk>
2022-04-25 20:03:01 +0200

    new commit

--- src/custom-axios/axios.js
+++ src/custom-axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:9090/api',
+    baseURL: 'https://mysterious-citadel-07163.herokuapp.com/api',
     headers: {
         'Access-Control-Allow-Origin' : '*'
     }
